389233ffb87f92ce8cf81ab1a180e9725750b4d0
tiinakoo <tiina.blakley@yle.fi>
2022-09-03 20:13:04 +0300

    Show form & goals

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,101 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import App from "./App";
+import getGoals from "./api/getGoals/getGoals";
+import { act } from "react-dom/test-utils";
 
-test("renders header", () => {
-  render(<App />);
+jest.mock("./api/getGoals/getGoals");
 
-  expect(screen.getByText("Goal organizer")).toBeInTheDocument();
+const getGoalsMock = getGoals as jest.Mock;
+
+describe("App", () => {
+  const goalsMock = {
+    data: [
+      { id: "123", goal: "Some goal", month: 2 },
+      { id: "124", goal: "Some other goal", month: 3 },
+    ],
+  };
+
+  it("renders header", async () => {
+    const promise = getGoalsMock.mockImplementation(() =>
+      Promise.resolve(goalsMock)
+    );
+
+    render(<App />);
+
+    expect(screen.getByText("Goal organizer")).toBeInTheDocument();
+
+    await act(async () => {
+      await promise;
+    });
+  });
+
+  it("should get data for goals", async () => {
+    const promise = getGoalsMock.mockImplementation(() =>
+      Promise.resolve(goalsMock)
+    );
+    render(<App />);
+
+    expect(getGoalsMock).toHaveBeenCalled();
+
+    await act(async () => {
+      await promise;
+    });
+  });
+
+  it("should show goals when there are goals", async () => {
+    getGoalsMock.mockImplementation(() => Promise.resolve(goalsMock));
+
+    render(<App />);
+
+    await waitFor(() => {
+      expect(screen.getByText("Goals")).toBeInTheDocument();
+    });
+  });
+
+  it("should not show goals when there are no goals", async () => {
+    getGoalsMock.mockImplementation(() =>
+      Promise.resolve({
+        data: [],
+      })
+    );
+
+    render(<App />);
+
+    await waitFor(() => {
+      expect(screen.queryByText("Goals")).not.toBeInTheDocument();
+    });
+  });
+
+  describe("when getting goals resolves with error", () => {
+    it("should fail with an error", async () => {
+      const promise = getGoalsMock.mockImplementation(() =>
+        Promise.reject("Error")
+      );
+
+      render(<App />);
+
+      await act(async () => {
+        await promise;
+      });
+
+      await expect(getGoalsMock).rejects.toMatch("Error");
+    });
+
+    it("should show error message", async () => {
+      const promise = getGoalsMock.mockImplementation(() =>
+        Promise.reject("Error")
+      );
+
+      render(<App />);
+
+      await act(async () => {
+        await promise;
+      });
+
+      await expect(
+        screen.getByText("Something went wrong when getting goals")
+      ).toBeInTheDocument();
+    });
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import AddGoal from "./components/AddGoal/AddGoal";
+import Goals from "./components/Goals/Goals";
+import { Goal } from "./types";
+import getGoals from "./api/getGoals/getGoals";
 
 function App() {
+  const [goals, setGoals] = useState<Array<Goal>>([]);
+  const [error, setError] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    getGoals()
+      .then((res) => {
+        setGoals(res.data);
+      })
+      .catch(() => setError("Something went wrong when getting goals"));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">Goal organizer</header>
+    <div className="app">
+      <header className="header">Goal organizer</header>
+
+      {error && <p className="error">{error}</p>}
+
+      {!error && (
+        <>
+          <AddGoal />
+
+          {goals.length > 0 && <Goals goals={goals} />}
+        </>
+      )}
     </div>
   );
 }
